6df16ac6f4d72593fd25c8bfc51c00b543ae505d
Anandarup Banerjee <anandarupbanerjee21@gmail.com>
2026-04-23 12:35:47 +0530

    tets

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Comment these only when serving React build from FastAPI
+  // Comment these only when serving React build from FastAPI code
   baseURL: "https://risk-analysis-gtczgeh0gse2f5a5.southindia-01.azurewebsites.net",
   // Uncomment these only when serving React build from FastAPI
   // baseURL: '',
