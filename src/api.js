56610e10fdb17460a703026b0b2212e5709e7ed7
Anandarup Banerjee <anandarupbanerjee21@gmail.com>
2026-04-28 18:27:38 +0530

    fixed base url

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // Comment these only when serving React build from FastAPI code
-  // baseURL: "https://risk-analysis-gtczgeh0gse2f5a5.southindia-01.azurewebsites.net",
-  baseURL: "http://localhost:8000",
+  baseURL: "https://risk-analysis-gtczgeh0gse2f5a5.southindia-01.azurewebsites.net",
+  // baseURL: "http://localhost:8000",
   
   // Uncomment these only when serving React build from FastAPI
   // baseURL: '',
