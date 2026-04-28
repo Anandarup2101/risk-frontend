d9bf3fb4ccdbfc20fefdd9583c10943ef4801e1a
Anandarup Banerjee <anandarupbanerjee21@gmail.com>
2026-04-28 19:37:13 +0530

    increased timeout

--- src/api.js
+++ src/api.js
@@ -4,9 +4,9 @@ const api = axios.create({
   // Comment these only when serving React build from FastAPI code
   baseURL: "https://risk-analysis-gtczgeh0gse2f5a5.southindia-01.azurewebsites.net",
   // baseURL: "http://localhost:8000",
-  
   // Uncomment these only when serving React build from FastAPI
   // baseURL: '',
+  timeout: 120000,
   headers: {
     'Content-Type': 'application/json',
   },
